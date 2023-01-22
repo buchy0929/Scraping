3e6e71a11a612a8f587f36cb6cd4750aa9bfb257
buchy0929 <buchy0929@gmail.com>
2023-01-22 22:39:37 +0900

    commit 20230122

--- ch1/download-png1.py
+++ ch1/download-png1.py
@@ -3,6 +3,7 @@ import urllib.request
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+
 # URLと保存パスを指定
 url = "https://uta.pw/shodou/img/28/214.png"
 savename = "test.png"
