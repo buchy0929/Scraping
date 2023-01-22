873638db1862b36512a15123ee3b077b6cae268f
buchy0929 <buchy0929@gmail.com>
2023-01-22 22:31:55 +0900

    commit

---
+++ ch1/download-png1.py
@@ -0,0 +1,13 @@
+# ライブラリの取り込み --- (※1)
+import urllib.request
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URLと保存パスを指定
+url = "https://uta.pw/shodou/img/28/214.png"
+savename = "test.png"
+
+# ダウンロード --- (※2)
+urllib.request.urlretrieve(url, savename)
+print("保存しました")
+
